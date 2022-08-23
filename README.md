184fef3a75baf27a8c5423c776c723cb8249e3f9
Ju777 <julien.tamil@gmail.com>
2022-08-23 10:58:26 +0200

    Decompose in many functions

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gilded Rose Kata : refactoring and testing
+This Kata was originally created by Terry Hughes (http://twitter.com/TerryHughes). It is already on GitHub here. See also Bobby Johnson's description of the kata.
+
+Whichever testing approach you choose, the idea of the exercise is to do some deliberate practice, and improve your skills at designing test cases and refactoring. The idea is not to re-write the code from scratch, but rather to practice designing tests, taking small steps, running the tests often, and incrementally improving the design.
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -7,60 +7,99 @@ class Item {
 }
 
 class Shop {
+
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
+
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+
+      var degradSpeed = this.getDegradSpeed(this.items[i]);
+      this.checkName(this.items[i], degradSpeed);
+      this.checkQualityLimits(this.items[i]);
+      this.updateSellIn(this.items[i]);
     }
 
     return this.items;
   }
+
+  getDegradSpeed = (item) => {
+    return item.sellIn <= 0 ? 2 : 1;
+  }
+
+  checkName = (item, degradSpeed) => {
+    switch(true) {
+      case (item.name === 'Aged Brie'):
+        console.log("On est dans le case Aged Brie.");
+        item.quality += 1;
+        break;
+      case (item.name === 'Sulfuras') :
+        console.log("On est dans le case Sulfuras.");
+        break;
+      case (item.name === 'Backstage passes to a TAFKAL80ETC concert') :
+        console.log("On est dans le case Backstage passes.");
+        switch(true) {
+          case (item.sellIn <= 10) :
+            item.quality += 2;
+            break;
+          case (item.sellIn <= 5) :
+            item.quality += 3;
+            break;
+          case (item.sellIn <= 0) :
+            item.quality = 0;
+            break;
+          default:
+            item.quality += 1;
+        }
+        break;
+      case (item.name.indexOf("Conjured") !== -1) :
+        item.quality -= degradSpeed*2;
+        console.log("On est dans le cas Conjured.");
+        break;
+      default:
+        console.log("On est dans le default");
+        item.quality -= 1*degradSpeed;
+    }
+
+  }
+
+  checkQualityLimits = (item) => {
+    if (item.quality < 0) { item.quality = 0 }
+    if (item.quality > 50 ) { item.quality = 50 }
+  }
+
+  updateSellIn = (item) => {
+    item.sellIn -= 1;
+  }
+
 }
 module.exports = {
   Item,
   Shop
 }
+
+
+// const perform = () => {
+//   console.log("test");
+
+//   const item1 = new Item("couteau", 30, 10);
+//   const item2 = new Item("fourchette", 20, 25);
+//   const item3 = new Item("Aged Brie", 20, 30);
+//   const item4 = new Item("Sulfuras", Infinity, 80);
+//   const item5 = new Item("Backstage passes to a TAFKAL80ETC concert", 30, 10);
+//   const item6 = new Item("Conjured something", 30, 25);
+
+//   const shop = new Shop([item1, item2, item3, item4, item5, item6]);
+
+//   // console.log(shop);
+
+//   for (let i = 0 ; i < 30 ; i++) {
+//     shop.updateQuality();
+//   }
+  
+//   console.log(shop);
+// }
+
+// perform();
